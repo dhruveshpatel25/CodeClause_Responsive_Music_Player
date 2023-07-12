44ba1d817157c3192ce51d7dc562b723a26a8089
Dhruvesh Patel <dhruveshpatel-cmpn@atharvacoe.ac.in>
2023-07-12 17:00:43 +0530

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "Lil Mama See",
+    artist: "Sultaan Dhillon, Road Runner ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dirty Little Secret",
+    artist: "Zack Knight, Nora Fatehi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Play Date",
+    artist: "Lilly Brooks",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Go Down Deh (feat. Shaggy and Sean Paul)",
+    artist: "Spice",
+    img: "music-4",
+    src: "music-4"
+  },
+];
